c0e3f24a9428375e44d789373eafc307199bdf06
gastyft <gastyftacla1@hotmail.com>
2023-03-02 08:30:46 -0300

    header v.0.1

--- carrito_proyect/src/app/app-routing.module.ts
+++ carrito_proyect/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path: '', 
+  redirectTo: 'principal', pathMatch:'full'},
+  {path: 'principal', 
+  component:PrincipalComponent }, 
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- carrito_proyect/src/app/app.module.ts
+++ carrito_proyect/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
