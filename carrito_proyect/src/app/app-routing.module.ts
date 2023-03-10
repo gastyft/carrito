b5e43d01a21cc18101a20d9ae9a7d95b0a56e08f
gastyft <gastyftacla1@hotmail.com>
2023-03-10 12:28:05 -0300

    actualizaciones

--- carrito_proyect/src/app/app-routing.module.ts
+++ carrito_proyect/src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
 import { Error404Component } from './error404/error404.component';
 import { LoginComponent } from './login/login.component';
+import { CardsComponent } from './cards/cards.component';
+import { CardsNotebookComponent } from './cards-notebook/cards-notebook.component';
+import { ContactoComponent } from './contacto/contacto.component';
 const routes: Routes = [
 
   {path: '', 
@@ -11,8 +14,12 @@ const routes: Routes = [
   component:PrincipalComponent }, 
   {path: 'login',
   component: LoginComponent},
-  
-
+  {path:'cards',
+component: CardsComponent},
+{path:'notebookcard',
+component: CardsNotebookComponent},  
+{path: 'contacto',
+component: ContactoComponent},
 
 
 
--- carrito_proyect/src/app/login/login.component.ts
+++ carrito_proyect/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { loginUsuario } from '../model/loginiusuario';
+import { TokenService } from '../services/token.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+ 
+  nombreUsuario!: string;
+  password!: string;
 
-  ngOnInit(): void {
+  isLogged= false;
+  isLoginFail = false;
+  loginUsuario: loginUsuario | undefined;
+  roles:string[]=[];
+  errMsj:string | undefined;
+  
+
+constructor(private authService: AuthService,   private router:Router,
+  private tokenService:TokenService ) { 
   }
 
+
+
+
+  ngOnInit(): void {
+
+    if(this.tokenService.getToken()){
+      this.isLogged=true;
+      this.isLoginFail=false;
+      this.roles = this.tokenService.getAuthorities();
+    }
+  }
+
+  onLogin():void {
+    this.loginUsuario= new loginUsuario(this.nombreUsuario, this.password);
+    this.authService.login(this.loginUsuario).subscribe(
+      data => {
+  this.isLogged= true;
+  this.isLoginFail=false;
+  this.tokenService.setToken(data.token);
+  this.tokenService.setUserName(data.nombreUsuario);
+  this.tokenService.setAuthorities(data.authorities);
+  this.roles=data.authorities;
+  this.router.navigate(['']);
+ },
+   err =>{
+  this.isLogged= false;
+  this.isLoginFail= true;
+  this.errMsj = err.error.mensaje=" Usuario o contraseña mal colocada. Intente de nuevo.";
+  console.log(this.errMsj);
+   }
+)}
+  
 }
