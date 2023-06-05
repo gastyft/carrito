0f37f91e3f023d28948d5233dcd8d2d018d303ce
gastyft <gastyftacla1@hotmail.com>
2023-06-05 17:46:37 -0300

    mensaje

--- carrito_proyect/src/app/principal/principal.component.ts
+++ carrito_proyect/src/app/principal/principal.component.ts
@@ -14,7 +14,7 @@ export class PrincipalComponent implements OnInit {
   ngOnInit(): void {
     const isWelcomeShown = localStorage.getItem('isWelcomeShown');
     
-    if (isWelcomeShown) {
+    if (!isWelcomeShown) {
       swal("Bienvenido a mi E-commerce", "Soy Desarrollador Full-Stack Jr y Tester Manual Trainee en busca de mi primer trabajo IT con ganas de trabajar y seguir aprendiendo en el mundo de la programación", "")
       .then(() => {
         swal("Bienvenido a mi E-commerce", "Falta implementacion de metodos de pago en seccion carrito", "success");
